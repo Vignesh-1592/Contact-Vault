64d5bc1863a7fd01f1510133d45a34ecd2918a18
Vignesh <s.vignesh1592@gmail.com>
2026-08-23 12:48:03 +0530

    Connect MongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 const PORT = 5000;
+
+connectDB();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Contact Vault Backend is running");
